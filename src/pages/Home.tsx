c42a3f1bc54457d1a22049c670a68e4fe9766a95
A1871034 <A1871034@adelaide.edu.au>
2024-07-28 02:23:38 +0930

    Cleanup from tauri base project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import { createSignal } from "solid-js";
 import { invoke } from "@tauri-apps/api/tauri";
 
-import logo from "../assets/logo.svg";
-
 const [greetMsg, setGreetMsg] = createSignal("");
 const [name, setName] = createSignal("");
 
@@ -14,19 +12,7 @@ async function greet() {
 function Home() {
     return (
         <div class="page_content">
-            <div class="row">
-            <a href="https://vitejs.dev" target="_blank">
-                <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-            </a>
-            <a href="https://tauri.app" target="_blank">
-                <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-            </a>
-            <a href="https://solidjs.com" target="_blank">
-                <img src={logo} class="logo solid" alt="Solid logo" />
-            </a>
-            </div>
-
-            <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
+            <p>Home page will go here</p>
 
             <form
             class="row"
